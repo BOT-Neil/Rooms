407c14758ba4929ec2a91406e8a19bc1039ac35d
trash <neilbooth125@gmail.com>
2022-03-18 16:44:42 +0200

    lbozo

---
+++ src/main/java/tel/endho/rooms/storage/Preset.java
@@ -0,0 +1,20 @@
+package tel.endho.rooms.storage;
+
+import org.bukkit.Material;
+
+public class Preset {
+    private String Name;
+    private String spawnlocString;
+    private String mainEnvironment;
+    private String mainBiome;
+    private String netherBiome;
+    private Material iconMaterial;
+    private String mainSchematic;
+    private String netherSchematic;
+    private String endSchematic;
+    private int fillHeight;
+    private Material mainFillMaterial;
+    private Material netherMaterial;
+    private Material endMaterial;
+    private Boolean fillBedrock;
+}
